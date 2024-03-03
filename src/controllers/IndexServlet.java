8348324e5750e5bcde79d2e12048cb9fb6e1a9d0
kyouhei.nakagawa <ii9uki1010@icloud.com>
2024-03-03 15:16:17 +0900

    newの作成

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -32,10 +33,14 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
-        List<tasks> messages = em.createNamedQuery("getAlltasks", tasks.class).getResultList();
-        response.getWriter().append(Integer.valueOf(messages.size()).toString());
+        List<tasks> tasks = em.createNamedQuery("getAlltasks", tasks.class).getResultList();
 
         em.close();
+
+        request.setAttribute("tasks", tasks);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");
+        rd.forward(request, response);
     }
 
 }
